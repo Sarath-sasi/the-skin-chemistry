74e95b6b668006f15759426f18eb9fa529643eac
Sarath-sasi <sarathinform@gmail.com>
2022-11-21 23:13:56 +0530

    project setup

--- index.php
+++ index.php
@@ -1,94 +1,6 @@
 <?php include('includes/header.php'); ?>
 <main class="home-page">
-<?php include('includes/banner.php'); ?>
-    
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="card mb-3" data-anim="fade-in">
-                    <div class="card-header">
-                        <h3>Buttons</h3>
-                    </div>
-                    <div class="card-body">
-                        <a href="" class="btn btn-primary">btn-primary</a>
-                        <a href="" class="btn btn-secondary">btn-secondary</a>
-                    </div>
-                </div>
-            </div>
-
-            <div class="col-lg-6" data-anim>
-                <div class="card mb-3" data-anim="fade-in">
-                    <div class="card-header">
-                        <h3>Heading</h3>
-                    </div>
-                    <div class="card-body">
-                        <h3 class="h1">.h1</h3>
-                        <h3 class="h2">.h2</h3>
-                        <h3 class="h3">.h3</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="card mb-3" data-anim>
-            <div class="card-header">
-                <h3>Admin Content area</h3>
-            </div>
-            <div class="card-body">
-                <div class="admin-content-area mt-5 mb-5">
-
-                    <p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href="#">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>
-
-                    <h2>Header Level 2</h2>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aliquid dolore consectetur laborum sit nesciunt doloribus repellat atque itaque sunt obcaecati harum labore, laudantium adipisci excepturi deserunt. Molestias, fugiat aliquid.</p>
-                    <ol>
-                        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                        <li>Aliquam tincidunt mauris eu risus.</li>
-                    </ol>
-
-                    <blockquote>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p>
-                    </blockquote>
-
-                    <h3>Header Level 3</h3>
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                        <li>Aliquam tincidunt mauris eu risus.</li>
-                    </ul>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aliquid dolore consectetur laborum sit nesciunt doloribus repellat atque itaque sunt obcaecati harum labore, laudantium adipisci excepturi deserunt. Molestias, fugiat aliquid.</p>
-
-                    <h4>Header Level 3</h4>
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                        <li>Aliquam tincidunt mauris eu risus.</li>
-                    </ul>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aliquid dolore consectetur laborum sit nesciunt doloribus repellat atque itaque sunt obcaecati harum labore, laudantium adipisci excepturi deserunt. Molestias, fugiat aliquid.</p>
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                        <li>Aliquam tincidunt mauris eu risus.</li>
-                    </ul>
-                    <h5>Header Level 3</h5>
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                        <li>Aliquam tincidunt mauris eu risus.</li>
-                    </ul>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aliquid dolore consectetur laborum sit nesciunt doloribus repellat atque itaque sunt obcaecati harum labore, laudantium adipisci excepturi deserunt. Molestias, fugiat aliquid.</p>
-                    <h6>Header Level 3</h6>
-                    <ul>
-                        <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                        <li>Aliquam tincidunt mauris eu risus.</li>
-                    </ul>
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum aliquid dolore consectetur laborum sit nesciunt doloribus repellat atque itaque sunt obcaecati harum labore, laudantium adipisci excepturi deserunt. Molestias, fugiat aliquid.</p>
-
-                </div>
-            </div>
-        </div>
-
-
-
-    </div>
-
+    <?php include('includes/banner.php'); ?>
 
     <section class="sc-skincare-sec bg-primary">
         <div class="container">
@@ -113,19 +25,111 @@
                             <div class="shape shape-three">
                                 <img src="./assets/images/shape-rect-stripe.png" class="w-100" alt="">
                             </div>
-                            <div class="shape shape-four"></div>
-                            <div class="shape shape-five"></div>
-                            <div class="shape shape-six"></div>
+                            <div class="shape shape-four">
+                                <img src="./assets/images/shape-circle-secondary.svg" class="w-100" alt="">
+                            </div>
+                            <div class="shape shape-five">
+                                <img src="./assets/images/shape-circle-secondary.svg" class="w-100" alt="">
+                            </div>
+                            <div class="shape shape-six">
+                                <img src="./assets/images/shape-circle-light.svg" class="w-100" alt="">
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-6">
                     <div class="content-wrap text-white">
                         <h2 class="head text-secondary">Skin Care Procedures</h2>
-                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat </p>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use
+                            a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat </p>
                         <ul class="list-style-star">
                             <li>There are many variations of passages</li>
                             <li>Many variations of passages</li>
+                            <li>Are many variations of passages</li>
+                            <li>There are many variations of passages</li>
+                            <li>Are many variations of passages</li>
+                        </ul>
+                        <a href="" class="btn btn-light">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sc-antiaging-sec">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-lg-6 order-2 order-lg-1">
+                    <div class="content-wrap">
+                        <h2 class="head text-primary">Anti aging Procedures </h2>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use
+                            a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum </p>
+                        <ul class="list-style-star">
+                            <li>Skin Treatments</li>
+                            <li>Laser Treatments</li>
+                            <li>Are many variations of passages</li>
+                            <li>There are many variations of passages</li>
+                            <li>Are many variations of passages</li>
+                        </ul>
+                        <a href="" class="btn btn-secondary">Read More</a>
+                    </div>
+                </div>
+                <div class="col-lg-6 order-1 order-lg-2">
+                    <div class="img-wrap">
+                        <div class="img">
+                            <img src="./assets/images/haircare-two.jpg" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="sc-skincare-sec bg-primary">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="img-wrap">
+                        <div class="round-img-wrap">
+                            <div class="pill pill-one">
+                                <img src="./assets/images/haircare-one.jpg" alt="">
+                            </div>
+                            <div class="pill pill-two">
+                                <img src="./assets/images/haircare-two.jpg" alt="">
+                            </div>
+
+
+                            <div class="shape shape-one">
+                                <img src="./assets/images/shape-circle-lg.svg" class="w-100" alt="">
+                            </div>
+                            <div class="shape shape-two">
+                                <img src="./assets/images/shape-rect-stripe.png" class="w-100" alt="">
+                            </div>
+                            <div class="shape shape-three">
+                                <img src="./assets/images/shape-rect-stripe.png" class="w-100" alt="">
+                            </div>
+                            <div class="shape shape-four">
+                                <img src="./assets/images/shape-circle-secondary.svg" class="w-100" alt="">
+                            </div>
+                            <div class="shape shape-five">
+                                <img src="./assets/images/shape-circle-secondary.svg" class="w-100" alt="">
+                            </div>
+                            <div class="shape shape-six">
+                                <img src="./assets/images/shape-circle-light.svg" class="w-100" alt="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="content-wrap text-white">
+                        <h2 class="head text-secondary">Hair Care</h2>
+                        <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use
+                            a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat </p>
+                        <ul class="list-style-star">
+                            <li>Skin Treatments</li>
+                            <li>Laser Treatments</li>
                             <li>Are many variations of passages</li>
                             <li>There are many variations of passages</li>
                             <li>Are many variations of passages</li>
