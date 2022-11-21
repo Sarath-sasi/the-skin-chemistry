0354a4d8ade3ba48484bbc30288bb323694806fb
Aneesh <brainchildofbalan@gmail.com>
2022-11-22 00:42:05 +0530

    chem style

--- includes/skin-chem.php
+++ includes/skin-chem.php
@@ -2,17 +2,18 @@
     <!-- <div class="container"> -->
         <div class="skin-chem-inner">
             <div class="row">
-                <div class="col-12 col-lg-6">
+                <div class="col-12 col-lg-7">
                     <div class="text-inner">
                         <h4>WELCOME TO THE SKIN CHEMISTRY</h4>
-                        <h1>Our Exquisite Centres at 
+                        <h1 class="head">Our Exquisite Centres at 
                             Dubai and other major cities</h1>
                             <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. 
 
                             </p>
+                            <a href="#" class="btn btn-light btn-chem">Read More</a>
                     </div>
                 </div>
-                <div class="col-12 col-lg-6">
+                <div class="col-12 col-lg-5">
                     <div class="image-wrapper">
                         <div class="image-inner">
                             <div class="image-outer-container">
