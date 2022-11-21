53ecce66e0ece9ef28066546b5de49e6855688c2
Sarath-sasi <sarathinform@gmail.com>
2022-11-21 20:55:03 +0530

    Footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,61 @@
 <footer class="footer">
+    <div class="container">
+        <div class="row">
+             <div class="col-xl-3 col-lg-3 col-md-3 ">
+                     <h3 class="title">OUR SERVICE</h3>
+                     <ul class="list-star">
+                         <li>
+                            <a href="#">Skin Care Procedures   </a>
+                         </li>
+                         <li>
+                            <a href="#">Skin Care Procedures   </a>
+                         </li>
+                         <li>
+                            <a href="#">Skin Care Procedures   </a>
+                         </li>
+                         <li>
+                            <a href="#">Skin Care Procedures   </a>
+                         </li>
+                     </ul>
+             </div>
+
+             <div class="col-xl-3 col-lg-3 col-md-3 ">
+                <h3 class="title">QUICK LINKS</h3>
+                <ul>
+                    <li>
+                       <a href="#">Skin Care Procedures   </a>
+                    </li>
+                    <li>
+                       <a href="#">Skin Care Procedures   </a>
+                    </li>
+                    <li>
+                       <a href="#">Skin Care Procedures   </a>
+                    </li>
+                    <li>
+                       <a href="#">Skin Care Procedures   </a>
+                    </li>
+                </ul>
+        </div>
+
+        <div class="col-xl-3 col-lg-3 col-md-3 ">
+            <h3 class="title">CONTACT</h3>
+            <ul>
+                <li>
+                   <a href="#">Skin Care Procedures   </a>
+                </li>
+                <li>
+                   <a href="#">Skin Care Procedures   </a>
+                </li>
+                <li>
+                   <a href="#">Skin Care Procedures   </a>
+                </li>
+                <li>
+                   <a href="#">Skin Care Procedures   </a>
+                </li>
+            </ul>
+    </div>
+        </div>
+    </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     <script src="assets/js/vendors/lazyload-all.js" async=""></script>
