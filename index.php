6d5b5ee7f3ac0a080f4b244770e38b683846abee
Sarath-sasi <sarathinform@gmail.com>
2022-11-22 00:42:32 +0530

    project setup

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
                     <div class="img-wrap">
                         <div class="round-img-wrap">
                             <div class="pill pill-one">
-                                <img src="./assets/images/haircare-one.jpg" alt="">
+                                <img src="./assets/images/cover-skin1.png" alt="">
                             </div>
                             <div class="pill pill-two">
-                                <img src="./assets/images/haircare-two.jpg" alt="">
+                                <img src="./assets/images/cover-skin2.png" alt="">
                             </div>
 
 
@@ -56,7 +56,11 @@
         </div>
     </section>
 
-    <section class="sc-antiaging-sec">
+    <section class="sc-antiaging-sec position-relative">
+
+        <div class="shape-main">
+            <img src="./assets/images/shape-ellipse-secondary.svg" class="w-100" alt="">
+        </div>
         <div class="container">
             <div class="row">
 
@@ -85,7 +89,7 @@
                                 <img src="./assets/images/shape-half-circle.svg" class="w-100" alt="">
                             </div>
                             <div class="img">
-                                <img src="./assets/images/haircare-two.jpg" alt="">
+                                <img src="./assets/images/cover-face-massage.png" alt="">
                             </div>
                         </div>
                     </div>
@@ -143,6 +147,52 @@
                             <li>Are many variations of passages</li>
                         </ul>
                         <a href="" class="btn btn-light">Read More</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sc-doctor-sec">
+        <div class="shape-main">
+            <img src="./assets/images/shape-ellipse-secondary.svg" class="w-100" alt="">
+        </div>
+        <div class="container">
+
+            <div class="head-wrap text-center">
+                <h2 class="head text-success">Meet our Doctor</h2>
+                <p class="p-main">There are many variations of passages of Lorem Ipsum available, but the </p>
+            </div>
+
+            <div class="testimonial-wrap">
+
+                <div class="row align-items-center">
+                    <div class="col-lg-6 order-2 order-lg-1">
+                        <div class="content-wrap">
+                            <div class="icon-quote">
+                                <img src="./assets/images/icon-quote.png" class="w-100" alt="">
+                            </div>
+                            <p>
+                                There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum,
+                                you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum you need to
+                                be sure there isn't anything embarrassing
+                            </p>
+
+                            <div class="author">
+                                <h5 class="text-success">Doctor Name</h5>
+                                <p>There are many variations </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 order-1 order-lg-2">
+                        <div class="img-wrap">
+                            <div class="doctor-wrap">
+                                <img src="./assets/images/doctor.png" alt="">
+                                <div class="call ripple ">
+                                    <img src="./assets/images/icon-call.png" class="w-100" alt="">
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
