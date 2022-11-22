c80c2b8a6724f9c268a1986a7f98c9bfcc9d8c46
Sarath-sasi <sarathinform@gmail.com>
2022-11-22 07:54:29 +0530

    Fav icon

--- includes/happy-clients.php
+++ includes/happy-clients.php
@@ -57,7 +57,7 @@
 
 
 <!-- video popup -->
-<!-- <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog video-popup ">
       <div class="modal-content">
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"><span class="close"></span></button>
@@ -67,4 +67,4 @@
         
       </div>
     </div>
-  </div> -->
+  </div> 
