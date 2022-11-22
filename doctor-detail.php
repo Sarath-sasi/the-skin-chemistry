f9b50ed5b9febae4f85eb42b616d7550a42fe5af
Aneesh <brainchildofbalan@gmail.com>
2022-11-23 01:37:17 +0530

    styling 2 pages

---
+++ doctor-detail.php
@@ -0,0 +1,136 @@
+<?php include('includes/header.php'); ?>
+<main class="home-page">
+
+    <section class="doctor-detail-wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-lg-7">
+                    <div class="doctor-main-wrapper">
+                        <div class="doctor-wrapper">
+                            <div class="row">
+                                <div class="col-xl-6">
+                                    <div class="image-wrapper">
+                                        <div class="image-container">
+                                            <img src="./assets/images/detail-doctor/image-dr-01.jpg" alt="">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-xl-6">
+                                    <div class="head-wrapper">
+                                        <h3 class="head"> John Samuel</h3>
+                                        <h5 class="sub-head">Dept of Dermetology</h5>
+                                    </div>
+                                    <div class="list-wrapper">
+                                        <div class="list-item">
+                                            <div class="list-data">Designation</div>
+                                            <div class="list-data">HOD, Dermetology</div>
+                                        </div>
+                                        <div class="list-item">
+                                            <div class="list-data">Phone</div>
+                                            <div class="list-data"><a href="tel:+9123456780">+9123456780</a></div>
+                                        </div>
+                                        <div class="list-item">
+                                            <div class="list-data">Email ID</div>
+                                            <div class="list-data"><a
+                                                    href="mailto:john@example.com">john@example.com</a></div>
+                                        </div>
+                                        <div class="list-item">
+                                            <div class="list-data">Specialisation</div>
+                                            <div class="list-data">Skincare speacialist</div>
+                                        </div>
+                                        <div class="list-item">
+                                            <div class="list-data">Experience</div>
+                                            <div class="list-data">10+ Years</div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="content-wrapper">
+                            <div class="bio-wrapper">
+                                <h3 class="head">
+                                    Biography
+                                </h3>
+                                <p class="para">
+                                    There are many variations of passages of Lorem Ipsum available, but the majority
+                                    have suffered alteration in some form, by injected humour, or randomised words which
+                                    don't look even slightly believable. If you are going to Lorem Ipsum you need to be
+                                    sure there isn't anything embarrassing some form, by injected humour, or
+                                    rabelievable. If you are going to Lorem Ipsum you need to be sure there isn't
+                                    anything embarrassing
+                                </p>
+                            </div>
+                        </div>
+                        <div class="extra-details-wrapper">
+                            <h3 class="head">
+                                Education + Trainings
+                            </h3>
+                            <p>
+                                There are many variations of passages of Lorem Ipsum available, but the majority have
+                                suffered believable.going to Lorem Ipsum you need to be sure there isn't anything
+                                embarrassing some form, by injected humour
+                            </p>
+                            <ul>
+                                <li>Nemo ipsam egestas volute and turpis dolores quaerat</li>
+                                <li>An enim nullam tempor at pretium purus blandit</li>
+                                <li>Nemo ipsam egestas volute and turpis dolores quaerat</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 col-lg-5">
+                    <div class="time-available">
+                        <div class="col-12">
+                            <div class="head-wrapper">
+                                <h3 class="head">Time Availability</h3>
+                            </div>
+                            <div class="list-wrapper">
+                                <div class="list-item">
+                                    <div class="list-data">Monday</div>
+                                    <div class="list-data">9:00 am - 5:00 pm</div>
+                                </div>
+                                <div class="list-item">
+                                    <div class="list-data">Tueday</div>
+                                    <div class="list-data">9:00 am - 5:00 pm</div>
+                                </div>
+                                <div class="list-item">
+                                    <div class="list-data">Wednesday</div>
+                                    <div class="list-data">9:00 am - 5:00 pm</div>
+                                </div>
+                                <div class="list-item">
+                                    <div class="list-data">Thursday</div>
+                                    <div class="list-data">9:00 am - 5:00 pm</div>
+                                </div>
+                                <div class="list-item">
+                                    <div class="list-data">Friday</div>
+                                    <div class="list-data">9:00 am - 5:00 pm</div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div class="btn-wrapper">
+                            <a href="" class="btn btn-light">Book Appointment</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+</main>
+
+<?php include('includes/footer.php'); ?>
+<!-- pagescript here... -->
+
+
+
+
+<script></script>
+<!-- End pagescript here... -->
+</body>
+
+</html>
