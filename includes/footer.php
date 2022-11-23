f5f2435a27cb695343d754a6964658cb1886bc9e
Sarath-sasi <sarathinform@gmail.com>
2022-11-23 08:08:59 +0530

    new changes

--- includes/footer.php
+++ includes/footer.php
@@ -1,86 +1,86 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-             <div class="col-xl-3 col-lg-4 col-md-4 ">
-                     <h3 class="title">OUR SERVICE</h3>
-                     <ul class="list-star">
-                         <li>
-                            <a href="#">Skin Care Procedures   </a>
-                         </li>
-                         <li>
-                            <a href="#">Anti aging Procedures    
+            <div class="col-xl-3 col-lg-4 col-md-4 ">
+                <h3 class="title">OUR SERVICE</h3>
+                <ul class="list-star">
+                    <li>
+                        <a href="#">Skin Care Procedures   </a>
+                    </li>
+                    <li>
+                        <a href="#">Anti aging Procedures    
                             </a>
-                         </li>
-                         <li>
-                            <a href="#">Hair Care
+                    </li>
+                    <li>
+                        <a href="#">Hair Care
                             </a>
-                         </li>
-                         <li>
-                            <a href="#">Laser Treatments  </a>
-                         </li>
-                     </ul>
-             </div>
+                    </li>
+                    <li>
+                        <a href="#">Laser Treatments  </a>
+                    </li>
+                </ul>
+            </div>
 
-             <div class="col-xl-3 col-lg-4 col-md-4 ">
+            <div class="col-xl-3 col-lg-4 col-md-4 ">
                 <h3 class="title">QUICK LINKS</h3>
                 <ul>
                     <li>
-                       <a href="#">Home   </a>
+                        <a href="#">Home   </a>
                     </li>
                     <li>
-                       <a href="#">About US  </a>
+                        <a href="#">About US  </a>
                     </li>
                     <li>
-                       <a href="#">Doctors  </a>
+                        <a href="#">Doctors  </a>
                     </li>
                     <li>
-                       <a href="#">Blog  </a>
+                        <a href="#">Blog  </a>
                     </li>
                     <li>
                         <a href="#">Contact US </a>
-                     </li>
+                    </li>
                 </ul>
-        </div>
+            </div>
 
-        <div class="col-xl-3 col-lg-4 col-md-4">
-            <h3 class="title">CONTACT</h3>
-             <p class="address">Level 12, Rolex Tower,Sheikh Zayed 
-                Road,Near Financial Centre Metro 
-                Station, Kochi</p>
-             <p><a href="mailto:info@theskinchemistry.com">info@theskinchemistry.com</a></p>
-        </div>
-        <div class="col-xl-3 col-lg-4 col-md-4 ">
-             <button class="btn btn-light">Book Appointment</button>
-             <p>P.<a href="tel:+91 0234 567 89"> +91 0234 567 89</a></p>
-             <div class="social">
-                  <a href="#"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="9" height="17" viewBox="0 0 9 17">
+            <div class="col-xl-3 col-lg-4 col-md-4">
+                <h3 class="title">CONTACT</h3>
+                <p class="address">Level 12, Rolex Tower,Sheikh Zayed Road,Near Financial Centre Metro Station, Kochi</p>
+                <p><a href="mailto:info@theskinchemistry.com">info@theskinchemistry.com</a></p>
+            </div>
+            <div class="col-xl-3 col-lg-4 col-md-4 ">
+                <button class="btn btn-light">Book Appointment</button>
+                <p>P.<a href="tel:+91 0234 567 89"> +91 0234 567 89</a></p>
+                <div class="social">
+                    <a href="#"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="9" height="17" viewBox="0 0 9 17">
                     <image id="facebook" width="9" height="17" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAkAAAARCAYAAAAPFIbmAAAABHNCSVQICAgIfAhkiAAAALNJREFUKFNj/P//PwMWwAEUEwdiRiB+y4imSBIoOB2IfYCYGap5HrqiE0AJczSTFyIr0gdKXkBS8AHI/gjEi5AVhQMFVkAVfQPSQkD8E8RHVpQA5M+HKgKZIAAzFaQoFshxBmIVILaGSvwB0luh7GUgRQuAnHg0xyJzJxKtyAuozQyIDYDYH2oEyMEdUPZeoh0Osx+v7wZIUQzQ3sVQu98CaRHkEIexWYEMaSjnO5B+CZMAAB4GV14WPYBBAAAAAElFTkSuQmCC"/>
                   </svg>
                   </a>
-                  <a href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="13" height="10" viewBox="0 0 13 10">
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="13" height="10" viewBox="0 0 13 10">
                         <image id="twitter" width="13" height="10" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA0AAAAKCAYAAABv7tTEAAAABHNCSVQICAgIfAhkiAAAANdJREFUKFNj/P//vxwDBDyC0jCKGcjoAuIkIP4PxLOA+AYQv2UEasoCMiqAOBqIDyNpLACy+9EM+gbkN4A0BQIZ66CSq4H0XCA+AaWD0TRdBPKtQZpMgIzlQKyCpuAPkM+CJgYyMAWkyQ7IOIgmiYtbCJSYANIEUlAJxNVAzI1H81+oax7ANHkDBTZgcQ6yGTOBnAyQAEwTiB0DxCVArI/FNnAAAPFXmCYdIEMQiJ2AOBOIxdE0bQLyQXH1FiYOsgkUaiDFbkCsBLIdiJ8BMSjOFgPxPnSbAeorQLqIjPERAAAAAElFTkSuQmCC"/>
                       </svg>
-                      
-                  </a>
-                  <a href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="13" height="13" viewBox="0 0 13 13">
+
+                    </a>
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="13" height="13" viewBox="0 0 13 13">
                         <image id="linkedin" width="13" height="13" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA0AAAANCAYAAABy6+R8AAAABHNCSVQICAgIfAhkiAAAAMFJREFUKFNj/P//PzMDA4MnEL8G4pNATBAwAjXVAFU1Q1VaAenjhHSBNDUCFdVBFdoB6cPEaGIFKgoE4pdAfJCQBpA8yCYPIA3CINANxCpQQ0D8OUAcAMROQPwEiNuB+DpIUwOQUQ/VZAikHYC4H8p/CqSlkWz/AGRrEtJ0H6ioF2qbC1RzKSFNYUCFq4FYDYhvQjXNIqTJEajwABALAPF7qKaFQ0CTBtCtIAwC+4FYBIh1ofwjQPoNEIMSgDdU7BEAdSlXvg3W/zcAAAAASUVORK5CYII="/>
                       </svg>
-                      
-                  </a>
-                  <a href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="15" viewBox="0 0 15 15">
+
+                    </a>
+                    <a href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="15" height="15" viewBox="0 0 15 15">
                         <image id="instagram" width="15" height="15" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAA8AAAAPCAYAAAA71pVKAAAABHNCSVQICAgIfAhkiAAAAT1JREFUKFOd0z1Iw0AYxvGmQkHqR0E72KGbUxd1EQq6KjgUUahzQcTVQXSzk10dxEkXHURQcBOcpFRc/Bq6ObiIoItVW1DE+H/CW4iRYmjgx3tJ7sldLhfHdd0IRxdWMIMBdCOqG3Z8U9/wiEOU8O4QVvAcPdhCVTegp9bQC8cGyFAX8YqswkUaBYzg2TdasBnjwhQuUcGOwrc09rEe6K0R07iHpjyNIyygD3MKf9DI4cTCndQNm00H9QubWEMeBxjFscJ6t3GULbxNncA8LjAGXdvFkvVR/7NmeJiTG/TjycKn1lFlFnvQdOsYwnUwrEXTgsTR8IVTtB8wiLtW4bZGbvudw672MtP9tFeZpHqrHfY7+5YgssqJ953D7rBmOEnjCt4Oa7W31fkFCUup35+9rXv//VXaSPoZfv1VP6gciYyGs+s1AAAAAElFTkSuQmCC"/>
                       </svg>
-                      
-                  </a>
-             </div>
-        </div>
+
+                    </a>
+                </div>
+            </div>
         </div>
         <div class="footer-btm">
-               <p>Copyright © 2022 The Skin Chemistry. All Rights Reserved.</p>
-               <p class="color-yellow">Designed By: <a href="#"> <img src="assets/images/inter-smart.png" /></a></p>
+            <p>Copyright © 2022 The Skin Chemistry. All Rights Reserved.</p>
+            <p class="color-yellow">Designed By:
+                <a href="#"> <img src="assets/images/inter-smart.png" /></a>
+            </p>
         </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
@@ -94,11 +94,12 @@
 
 
 
-         <script type="text/javascript" src="assets/js/vendors/swiper-bundle.js"></script>
+    <script type="text/javascript" src="assets/js/vendors/swiper-bundle.js"></script>
+    <script type="text/javascript" src="assets/js/vendors/glightbox.min.js"></script>
     <script type="text/javascript" src="assets/js/main.js"></script>
 
 
-  
+
 
 
 </footer>
