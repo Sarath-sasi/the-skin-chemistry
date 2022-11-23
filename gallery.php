617db111c80490abcc0940ae20839440794ca5f9
Sarath-sasi <sarathinform@gmail.com>
2022-11-23 23:54:07 +0530

    gallery-page, galler-detail,news-detail pages

---
+++ gallery.php
@@ -0,0 +1,157 @@
+<?php include('includes/header.php'); ?>
+<main class="inner-pages">
+<section class="inner-banner">
+    <div class="inner-banner-image">
+        <img src="assets/images/gallery/gallery-banner.png" />
+        <div class="container">
+            <div class="banner-content">
+                <h2 class="head text-white mb-2">Gallery</h2>
+                <p>There are many variations of passages of Lorem Ipsum available.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="gallery-wrapper-main gallery-main-sec">
+    <div class="container">
+        <div class="gallery-inner">
+            <div class="gallery-item-wrapper">
+                <div class="row">
+                    <div class="col-6 col-lg-6">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-l glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div><div class="col-6 col-lg-6">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-l glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-6 col-lg-3">
+                        <a href="./assets/images/gallery/image-l.jpg" class="gallery-wrapper image-s glightbox">
+                            <div class="gallery-image">
+                                <div class="gallery-container">
+                                    <img src="./assets/images/gallery/image-l.jpg" alt="">
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include('includes/footer.php'); ?>
+<!-- pagescript here... -->
+<script></script>
+<!-- End pagescript here... -->
+</body>
+
+</html>
---
+++ news-detail.php
@@ -0,0 +1,109 @@
+<?php include('includes/header.php'); ?>
+<main class="inner-pages">
+
+<section class="news-wrap news-detail-sec">
+     <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-xl-8 col-lg-8">
+                    <div class="news-list">
+                        <h3 class="head text-primary">Doctors Aesthetics Centre offers you with a variety of treatment</h3>
+                        <p class="news-list-detail">
+                            <span class="date">Post Date:
+                                <span>Jan 21,2022</span>
+                            </span> 
+                            /
+                            <span class="post">Post By:
+                                <span>Admin</span>
+                            </span>
+                        </p>
+                        <div class="news-detail-img">
+                            <div class="img-container">
+                                <img src="assets/images/gallery/news-detail-img.png" alt="">
+                            </div>
+                        </div>
+                        <div class="news-content">
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisici elit, sed eiusmod tempor incidunt ut labore et dolore
+                                magna aliqua. Idque Caesaris facere voluntate liceret: sese habere. Magna pars studiorum, prodita
+                                quaerimus. Magna pars studiorum, prodita quaerimus. Fabio vel iudice vincam, sunt in culpa qui officia.
+                                Vivamus sagittis lacus vel augue laoreet rutrum faucibusmagna aliqua. Idque Caesaris facere voluntate
+                                liceret: sese habere. Magna pars studiorum, prodita quaerimus. Magna pars studiorum, prodita quaerimus.
+                                Fabio vel iudice vincam, sunt in culpa qui officia. Vivamus sagittis lacus vel augue laoreet rutrum
+                                faucibus
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                 <div class="col-md-12 col-xl-4 col-lg-4">
+                    <h3 class="head text-primary">Upcoming Treatments</h3>
+                       <div class="news-list right">
+                        <ul>
+                            <li>
+                                <a href="" class="list-item">
+                                    <div class="head-image">
+                                        <figure>
+                                            <img src="assets/images/news4.jpg" />
+                                        </figure>
+                                    </div>
+                                    <div class="dec">
+                                       
+                                         <h3>Doctors Aesthetics Centre offers 
+                                          you with a variety of treatment</h3>
+                                          <div class="date">JAN 21,2022</div>
+                                         
+                                    </div>
+                                  </a>
+                            </li>
+                            <li>
+                                <a href="" class="list-item">
+                                    <div class="head-image">
+                                        <figure>
+                                            <img src="assets/images/news3.jpg" />
+                                        </figure>
+                                    </div>
+                                    <div class="dec">
+                                       
+                                         <h3>Doctors Aesthetics Centre offers 
+                                          you with a variety of treatment</h3>
+                                          <div class="date">JAN 21,2022</div>
+                                         
+                                    </div>
+                                  </a>
+                            </li>
+                            <li>
+                                <a href="" class="list-item">
+                                    <div class="head-image">
+                                        <figure>
+                                            <img src="assets/images/news1.jpg" />
+                                        </figure>
+                                    </div>
+                                    <div class="dec">
+                                       
+                                         <h3>Doctors Aesthetics Centre offers 
+                                          you with a variety of treatment</h3>
+                                          <div class="date">JAN 21,2022</div>
+                                         
+                                    </div>
+                                  </a>
+                            </li>
+                        
+
+                         
+
+                          
+                            
+                            </ul>
+                       </div>
+                 </div>
+            </div>
+     </div>
+</section>
+
+<?php include('includes/footer.php'); ?>
+<!-- pagescript here... -->
+<script></script>
+<!-- End pagescript here... -->
+</body>
+
+</html>
